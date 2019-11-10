244d2af28a62f36ba291151c58160c6a78ff3a58
Joshua Lee <joshua@aquila.local>
2019-11-10 14:51:32 -0500

    order updates

---
+++ Downloads/447_proj-master/server/models/Order.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var OrderSchema = new Schema({
+	FoodItem: String,
+	CustomerName: String,
+	PhoneNumber: String,
+	TimeStamp: Date,
+	PickupOrDelivery: Boolean,
+	Address: String,
+	PickUpDeliveryTime: String,
+	Allergies: Array,
+	SpecialInstructions: String
+});
+
+
+var Order = mongoose.model('orders', OrderSchema);
+
+
+module.exports = Order;
---
+++ Downloads/447_proj-master/server/routes/orders.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+Orders = require('../models/Order');
+
+/* GET orders listing. */
+router.get('/', function(req, res, next) {
+  Orders.find()
+    .then(orders => {
+      res.json(orders);
+    })
+    .catch(err => {
+      console.log('GET ORDER ERROR: ', err);
+    })
+});
+
+/* POST order. */
+router.post('/', (req, res, next) => {
+  const newOrder = new Orders({
+    FoodItem: req.body.food,
+    CustomerName: req.body.name,
+    PhoneNumber: req.body.phone,
+    PickupOrDeliver: req.body.pickOrDel,
+    Address: req.body.address
+  })
+  newOrder
+    .save()
+    .then(order => {
+      res.json({order, msg: 'Success'})
+    })
+    .catch(err => {
+      console.log('CREATE ORDER ERROR: ', err);
+    });
+})
+
+module.exports = router;
